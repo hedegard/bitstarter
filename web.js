888e7fde6554829eaa4c11dd457ef37b10c8f0bf
Leif Hedegard <hedegard@kth.se>
2013-07-18 08:20:05 +0000

    new file:   index.html 	modified:   web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var infoToShow  = "testing";
 
+app.get('/', function(request, response) {
+  response.send(infoToShow);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
