2fc3f83bcf39db0ae428eb583ebb98ba57f20b2d
Leif Hedegard <hedegard@kth.se>
2013-07-18 08:39:05 +0000

    modified:   web.js

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ var infoToShow  = "testing";
 
 app.get('/', function(request, response) {
   response.send("infoToShow");
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
