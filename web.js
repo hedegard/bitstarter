cb80a731931ea85f6eaf8519094f9e8a815b7a48
Leif Hedegard <hedegard@kth.se>
2013-07-18 08:26:18 +0000

    modified:   web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var infoToShow  = "testing";
 
 app.get('/', function(request, response) {
-  response.send(infoToShow);
+  response.send("infoToShow");
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
